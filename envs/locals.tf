5d7d9786c92cf0f6b7cb84af1f115d2f8542ab93
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-14 21:48:26 +0900

    :memo: security_zones 追加

--- envs/locals.tf
+++ envs/locals.tf
@@ -7,3 +7,13 @@ locals {
     r.key => r.name
   }
 }
+
+/************************************************************
+Security Zones Maximum recipe
+************************************************************/
+locals {
+  oracle_recipe_id = distinct([
+    for r in data.oci_cloud_guard_security_recipes.maximum.security_recipe_collection[0].items : r.id
+    if r.owner == "ORACLE"
+  ])[0]
+}
