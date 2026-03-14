82a6b643b7d87ef0d04713b6b30d926753012fcd
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-14 20:47:50 +0900

    :memo: compartments名修正

--- envs/compartments.tf
+++ envs/compartments.tf
@@ -1,17 +1,9 @@
 /************************************************************
 Compartment - workload
 ************************************************************/
-resource "oci_identity_compartment" "parent" {
+resource "oci_identity_compartment" "workload" {
   compartment_id = var.tenancy_ocid
-  name           = "Parent"
-  description    = "Parent For oci-security-zones-preventive-controls"
+  name           = "oci-security-zones-preventive-controls"
+  description    = "Workload For oci-security-zones-preventive-controls"
   enable_delete  = true
-}
-
-resource "oci_identity_compartment" "child" {
-  compartment_id = oci_identity_compartment.parent.id
-  name           = "Child"
-  description    = "Child For oci-security-zones-preventive-controls"
-  enable_delete  = true
-  defined_tags   = local.common_defined_tags
 }
