4ef8f3e341dc22b2c529c4e80d259613f59ba8e8
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-14 20:47:26 +0900

    :memo: Default Tags削除

--- envs/locals.tf
+++
@@ -1,9 +0,0 @@
-# /************************************************************
-# Common
-# ************************************************************/
-locals {
-  common_defined_tags = {
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
-  }
-}
