ee862a3d3e603f3450db2c51f1f163c84b3de3ef
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-15 18:00:48 +0900

    :bug: Security Zone 削除順番の依存関係を満たせないので方式を変更

--- envs/cloud_guard.tf
+++ envs/cloud_guard.tf
@@ -10,27 +10,3 @@ resource "oci_cloud_guard_cloud_guard_configuration" "this" {
   status                = "ENABLED" # ENABLED or DISABLED
   self_manage_resources = false
 }
-
-/************************************************************
-local-exec
-************************************************************/
-resource "terraform_data" "disable_cloud_guard" {
-  depends_on = [
-    oci_identity_policy.cloud_guard_policy
-  ]
-  input = {
-    tenancy_ocid = var.tenancy_ocid
-    region       = local.region_map["NRT"]
-  }
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<EOT
-      oci cloud-guard configuration update \
-      --compartment-id ${self.input.tenancy_ocid} \
-      --reporting-region ${self.input.region} \
-      --status DISABLED \
-      --profile ADMIN \
-      --auth security_token
-    EOT
-  }
-}
